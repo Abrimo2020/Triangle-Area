b07324089cda9bb7f8f80c0ed538d70db6a01710
Abrimo2020 <62276335+Abrimo2020@users.noreply.github.com>
2022-05-11 08:20:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriangleArea
+{
+    class Triangle
+    {
+        double baseA;
+        double height;
+        public void AcceptDetails()
+        {
+            //length = 2; width = 3;
+            Console.WriteLine("please Enter the base of the triangle:");
+            baseA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the heigt of the triangle:");
+            height = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+        public double getArea()
+        {
+            return (0.5*(baseA * height));
+        }
+        public void Display()
+        {
+            Console.WriteLine("The base of triangle:{0}", baseA);
+            Console.WriteLine("The heiht of triangle:{0}", height);
+            Console.WriteLine("The Area of Rectangle:{0}", getArea());
+
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle r = new Triangle();
+            r.AcceptDetails();
+            r.Display();
+            //r.Display();
+            Console.ReadKey();
+        }
+    }
+}
